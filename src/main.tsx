59ff6386d44b67bdce0985af428610c97703109c
herlinm4-prog <herlinm4@gmail.com>
2026-08-22 11:18:03 -0400

    Load Meevo onboarding styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './connection.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/NEXT-WALKINS/sw.js').catch(() => undefined);
-  });
-}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(()=>undefined));}
