541b00023dd3b86e659165f3b8095f5b18d6e490
herlinm4-prog <herlinm4@gmail.com>
2026-08-22 11:13:59 -0400

    Register service worker for installable app

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,9 @@ import App from './App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/NEXT-WALKINS/sw.js').catch(() => undefined);
+  });
+}
